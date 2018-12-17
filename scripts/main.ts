c9e6acf64231dca644da826835dd8ee53089b0b8
Nguyen Sang <nnsang24@gmail.com>
2018-12-17 17:35:47 +0700

    add news.html

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,9 +1,12 @@
-import { Header } from '../views/lib/header/index';
+// import { Header } from '../views/lib/header/index';
+import { MenuNews } from '../views/lib/nav/n-1/index';
 
 
-let header = new Header();
+// let header = new Header();
+let menunews = new MenuNews();
 
 $(document).ready(function () {
-	header.moveUser();
-	header.moveSearch();
+	// header.moveUser();
+	// header.moveSearch();
+	menunews.toggleNews();
 });
--- views/lib/header/index.ts
+++ views/lib/header/index.ts
@@ -1,22 +1,22 @@
-export class Header {
-	moveUser() {
-		let moveUser = new MappingListener({
-			selector: '.user',
-			desktopWrapper: '.toolbar .cart',
-			desktopMethod: 'insertAfter',
-			mobileWrapper: '.main-navbar .nav',
-			mobileMethod: 'prependTo',
-			breakpoint: 992,
-		}).watch()
-	};
-	moveSearch() {
-		let moveUser = new MappingListener({
-			selector: '.search',
-			desktopWrapper: '.toolbar .language',
-			desktopMethod: 'insertAfter',
-			mobileWrapper: '.menu-wrap .logo-wrap',
-			mobileMethod: 'insertAfter',
-			breakpoint: 992,
-		}).watch()
-	}
-}
+// export class Header {
+// 	moveUser() {
+// 		let moveUser = new MappingListener({
+// 			selector: '.user',
+// 			desktopWrapper: '.toolbar .cart',
+// 			desktopMethod: 'insertAfter',
+// 			mobileWrapper: '.main-navbar .nav',
+// 			mobileMethod: 'prependTo',
+// 			breakpoint: 992,
+// 		}).watch()
+// 	};
+// 	moveSearch() {
+// 		let moveUser = new MappingListener({
+// 			selector: '.search',
+// 			desktopWrapper: '.toolbar .language',
+// 			desktopMethod: 'insertAfter',
+// 			mobileWrapper: '.menu-wrap .logo-wrap',
+// 			mobileMethod: 'insertAfter',
+// 			breakpoint: 992,
+// 		}).watch()
+// 	}
+// }
