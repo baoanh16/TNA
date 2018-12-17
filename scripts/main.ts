1294efebd84fbb0a968aadb36abf39081dd021d0
sonvh2510 <sonvh2510@gmail.com>
2018-12-17 17:43:09 +0700

    almost done header 90%

--- scripts/main.ts
+++ scripts/main.ts
@@ -4,6 +4,18 @@ import { Header } from '../views/lib/header/index';
 let header = new Header();
 
 $(document).ready(function () {
+	header.moveLanguage();
+	header.moveCart();
 	header.moveUser();
 	header.moveSearch();
+	header.toggleSearch();
+	header.turnoffSearch();
+	header.toggleMainNav();
+	header.toggleCart();
+	header.closeCart();
+	let breakpoint = window.matchMedia("(max-width: 991.98px)")
+	header.addBackdrop(breakpoint);
+	breakpoint.addListener(header.addBackdrop);
+	header.closeMainNav();
+	header.toggleLang();
 });
--- views/lib/header/index.ts
+++ views/lib/header/index.ts
@@ -1,9 +1,29 @@
 export class Header {
+	moveLanguage() {
+		let moveLanguage = new MappingListener({
+			selector: '.language',
+			desktopWrapper: '.toolbar',
+			desktopMethod: 'appendTo',
+			mobileWrapper: '.main-navbar .nav',
+			mobileMethod: 'appendTo',
+			breakpoint: 992,
+		}).watch()
+	};
+	moveCart() {
+		let moveCart = new MappingListener({
+			selector: '.cart',
+			desktopWrapper: '.toolbar',
+			desktopMethod: 'appendTo',
+			mobileWrapper: '.main-navbar',
+			mobileMethod: 'insertBefore',
+			breakpoint: 992,
+		}).watch()
+	}
 	moveUser() {
 		let moveUser = new MappingListener({
 			selector: '.user',
-			desktopWrapper: '.toolbar .cart',
-			desktopMethod: 'insertAfter',
+			desktopWrapper: '.toolbar',
+			desktopMethod: 'appendTo',
 			mobileWrapper: '.main-navbar .nav',
 			mobileMethod: 'prependTo',
 			breakpoint: 992,
@@ -12,11 +32,62 @@ export class Header {
 	moveSearch() {
 		let moveUser = new MappingListener({
 			selector: '.search',
-			desktopWrapper: '.toolbar .language',
-			desktopMethod: 'insertAfter',
+			desktopWrapper: '.toolbar',
+			desktopMethod: 'appendTo',
 			mobileWrapper: '.menu-wrap .logo-wrap',
 			mobileMethod: 'insertAfter',
 			breakpoint: 992,
 		}).watch()
+	}
+
+	toggleSearch() {
+		$('.cc-header .search-toggle').on('click', function () {
+			$('.cc-header .search-panel').toggleClass('active')
+		})
+	}
+	turnoffSearch() {
+		$('.cc-header .search-turn-off').on('click', function (e) {
+			e.stopPropagation()
+			$(this).parent().removeClass('active')
+		})
+	}
+	toggleMainNav() {
+		$('.cc-header .menu-toggle').on('click', function () {
+			$('.cc-header .nav').toggleClass('active')
+			$('.cc-header .backdrop').fadeToggle(400)
+		})
+	}
+	closeMainNav() {
+		$('.backdrop').on('click', function () {
+			console.log(1);
+			$('.nav').removeClass('active')
+			$('.backdrop').fadeOut(400)
+
+		})
+	}
+
+	toggleCart() {
+		$('.cc-header .cart-toggle').on('click', function () {
+			$('.cc-header .cart-panel').toggleClass('active')
+		})
+	}
+
+	closeCart() {
+		$('.cc-header .cart-header span.fa-times').on('click', function () {
+			$('.cc-header .cart-panel').removeClass('active')
+		})
+	}
+
+	addBackdrop(x: any) {
+		if (x.matches) { // If media query matches
+			$('header').append('<div class="backdrop"></div>')
+		} else {
+			$('.backdrop').remove()
+		}
+	}
+	toggleLang() {
+		$('.cc-header .language').on('click', function () {
+			$(this).find('ul').slideToggle(400)
+		})
 	}
 }
